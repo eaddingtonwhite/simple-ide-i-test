309b192253d68fd845ca17fe276c6a7704407bd6
eaddingtonwhite <ellery_addington-white@intuit.com>
2020-02-19 10:51:04 -0800

    changed: simplified repo layout for simple IDE.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function inputHandler(e){
+   console.log('Text changed in text area. NewText:', e.target.value);
+}
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple IDE</h1>
+      <textarea rows={20} cols={60} placeholder={"Write some code!"} onInput={inputHandler}/>
     </div>
   );
 }
